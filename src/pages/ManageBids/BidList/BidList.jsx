2e81dc05af7cd15d00449d43ac994a9aed3bb9cf
Santu-Hansariafood <Santu@hansariafood.com>
2026-03-24 15:57:03 +0530

    fixed and update

--- src/pages/ManageBids/BidList/BidList.jsx
+++ src/pages/ManageBids/BidList/BidList.jsx
@@ -60,6 +60,11 @@ const BidList = () => {
     const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
 
     return bids.filter((bid) => {
+      // Filter by group if user is a buyer
+      if (userRole === "Buyer" && buyerGroup && bid.group !== buyerGroup) {
+        return false;
+      }
+
       const bidDateStr = (bid.bidDate && typeof bid.bidDate === 'string') ? bid.bidDate.split('T')[0] : "";
       let bidDate = null;
       if (bidDateStr) {
