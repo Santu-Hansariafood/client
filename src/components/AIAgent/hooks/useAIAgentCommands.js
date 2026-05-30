5072ba1f4b29c60dd95316914585cb8ea52148af
Santu-Hansariafood <Santu@hansariafood.com>
2026-05-30 10:30:44 +0530

    changes the ai with components

--- src/components/AIAgent/hooks/useAIAgentCommands.js
+++ src/components/AIAgent/hooks/useAIAgentCommands.js
@@ -1,5 +1,5 @@
 import { useRef } from "react";
-import dashboardData from "../../data/dashboardData.json";
+import dashboardData from "../../../data/dashboardData.json";
 
 export const useAIAgentCommands = ({
   input,
